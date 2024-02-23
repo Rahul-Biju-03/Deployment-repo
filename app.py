1fbf17776117b137781c08a929d944b95013a854
Rahul Biju <106422354+Rahul-Biju-03@users.noreply.github.com>
2024-02-23 15:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ if st.button("Predict type of Iris"):
 st.text('')
 st.text('')
 st.markdown(
-    '`Create by` [Intel-Unnati](https://github.com/intel-unnati-saintgits/intel-unnati-certificate-programme/blob/main/README.md) | \
+    '`Create by` [Rahul Biju](https://github.com/intel-unnati-saintgits/intel-unnati-certificate-programme/blob/main/README.md) | \
          `Code:` [GitHub](https://github.com/intel-unnati-saintgits/Model-Deployment-Demo/edit/main/app.py)')
